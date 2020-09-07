41b7a09768baa7f2badb8123ca8f552e57695840
afhassan <afayek@mun.ca>
2020-09-07 20:10:23 -0230

    Show details of a movie

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ function getMovies(searchText){
                 <div class="well text-center">
                     <img  class="py-2" src="${movie.Poster}">
                     <h5>${movie.Title} (${movie.Year})</h5>
+                    <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-secondary" href="#">Details</a>
                 </div>
             </div>
             `;
@@ -30,3 +31,49 @@ function getMovies(searchText){
     })
 }
 
+function movieSelected(id) {
+  sessionStorage.setItem('movieID', id);
+  window.location = 'show.html';
+  return false;
+}
+
+function showMovie(){
+  let movieId = sessionStorage.getItem('movieID');
+
+  axios.get('http://www.omdbapi.com/?apikey=91e54dfc&i=' + movieId)
+  .then(function(response){
+      console.log(response);
+      let movie = response.data;
+
+      let output =`
+        <div class="row">
+          <div class="col-md-4 py-5">
+            <img src="${movie.Poster}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+           <h2 class="display-4">${movie.Title}</h2>
+            <ul class="list-group">
+              <li class="list-group-item text-left"><strong>Genres:</strong> ${movie.Genre}</li>
+              <li class="list-group-item text-left"><strong>Language:</strong> ${movie.Language}</li>
+              <li class="list-group-item text-left"><strong>Released:</strong> ${movie.Released}</li>
+              <li class="list-group-item text-left"><strong>Director:</strong> ${movie.Director}</li>
+              <li class="list-group-item text-left"><strong>Writers:</strong> ${movie.Writer}</li>
+              <li class="list-group-item text-left"><strong>Actors:</strong> ${movie.Actors}</li>
+              <li class="list-group-item text-left"><strong>Rated:</strong> ${movie.Rated}</li>
+              <li class="list-group-item text-left"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+              <li class="list-group-item text-left"><strong>Plot:</strong> ${movie.Plot}</li>
+            </ul>
+            <div class="text-center py-3">
+              <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+              <a href="index.html" class="btn btn-secondary">Go Back To Search</a>
+            </div>
+          </div>
+        </div>
+      `;
+
+      $('#showMovie').html(output);
+  })
+  .catch(function(err){
+      console.log(err);
+  })
+}
